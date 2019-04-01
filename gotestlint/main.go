b320b20dec40a8131bd201dfb84a96552872a862
Maxim Zhiburt <zhiburt@gmail.com>
2019-04-01 21:25:20 +0300

    Add support for ./... param
    this param will trun up recursive walking

--- gotestlint/main.go
+++ gotestlint/main.go
@@ -16,23 +16,34 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatalln("don't have enought arguments")
 	}
-	path := os.Args[1]
 
-	files, err := parseFolder(path)
-	_handleError(err)
+	for _, path := range os.Args[1:] {
+		files, err := parseFolder(path)
+		_handleError(err)
 
-	l := lint.Linter{}
-	resp, err := l.LintFiles(files)
-	_handleError(err)
+		l := lint.Linter{}
+		resp, err := l.LintFiles(files)
+		_handleError(err)
 
-	for _, problem := range resp {
-		fmt.Println(problem)
+		for _, problem := range resp {
+			fmt.Println(problem)
+		}
 	}
 }
 
 var ParseFolderErr = errors.New("can't parse folder")
 
+const RECURSIVEPATH = "./..."
+
 func parseFolder(path string) (map[string][]byte, error) {
+	if path == RECURSIVEPATH {
+		return parse(".", true)
+	}
+
+	return parse(path, false)
+}
+
+func parse(path string, recursive bool) (map[string][]byte, error) {
 	files := make(map[string][]byte)
 	information, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -40,14 +51,22 @@ func parseFolder(path string) (map[string][]byte, error) {
 	}
 
 	for _, info := range information {
+		p := filepath.Join(path, info.Name())
 		if info.IsDir() {
+			if recursive {
+				m, err := parse(p, recursive)
+				if err != nil {
+					return nil, err
+				}
+
+				copyMapToMap(files, m)
+			}
 			continue
 		}
 		if strings.HasSuffix(info.Name(), ".go") == false {
 			continue
 		}
 
-		p := filepath.Join(path, info.Name())
 		body, err := ioutil.ReadFile(p)
 		if err != nil {
 			return nil, ParseFolderErr
@@ -56,6 +75,12 @@ func parseFolder(path string) (map[string][]byte, error) {
 	}
 
 	return files, err
+}
+
+func copyMapToMap(lhs, rhs map[string][]byte) {
+	for key, value := range rhs {
+		lhs[key] = value
+	}
 }
 
 func _handleError(err error) {
